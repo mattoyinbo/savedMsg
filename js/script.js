ca7c9a445b106658c4fb00cb33b43ddc8e2c60cd
mattoyinbo <mattoyinbo@gmail,com>
2019-07-01 15:26:48 -0400

    added set setimer function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-console.log('Hello world');
+( () => {
+
+    const form = document.getElementById('message-form');
+    form.addEventListener('submit', (event) => {
+    event.preventDefault(); //prevent default action
+
+    const msg = document.getElementById('message'); // get msg entered
+    const lastmsg = document.querySelector('.message-content');
+
+    // check if message is empty
+    if(msg.value === ''){
+        const feedback = document.querySelector('.feedback');
+        feedback.classList.add('show');
+        setTimeout(() =>{feedback.classList.remove('show')},3000);
+    }
+    else{
+    //change value of messge content class
+    lastmsg.textContent = msg.value;
+    msg.value = '';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    });
+
+})();
